b26d4bc2415387827d0bc97a71cb71eabeb1e15d
Кирилл Горбунов <kagorbunov_2@miem.hse.ru>
2021-07-22 02:23:44 +0300

    heroku 8

--- djangoProject/settings/production.py
+++ djangoProject/settings/production.py
@@ -144,6 +144,6 @@ STATIC_ROOT = (BASE_DIR / 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static' ),
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
