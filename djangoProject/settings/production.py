9879bec7f895c5484c0a0938e5f6d0eca55b2462
Кирилл Горбунов <kagorbunov_2@miem.hse.ru>
2021-07-22 03:55:33 +0300

    heroku 11

--- djangoProject/settings/production.py
+++ djangoProject/settings/production.py
@@ -19,7 +19,7 @@ DB_NAME = os.environ.get('DB_NAME')
 DB_PASSWORD = os.environ.get('DB_PASSWORD')
 DB_HOST = os.environ.get('DB_HOST')
 DB_USER = os.environ.get('DB_USER')
-SECRET_KEY = os.environ.get('SECRET_KEY')
+#SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY
+SECRET_KEY = 'django-insecure-(1+1j-wl(3*&p!*!9+r=6)nc_qzy_ekqbqm+1h1^-8v()y&q%v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
